ab7debac3964ebf655aa27ae94e048b02d2e1304
UnityTheCoder <108679939+UnityTheCoder@users.noreply.github.com>
2022-08-08 16:48:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import ui
+import os, re, sys
+
+
+
+def retrieveTitle(path):
+    result = os.popen(f"pdfinfo {path}  2>/dev/null | grep Title: | sed 's/Title:[ ]*//'")
+    return result.read().replace(r"\n", "\n")
+
+
+def retrieveContent(path):
+    result = os.popen(f"pdftotext {path} -")
+    return result.read().replace(r"\n", "\n")
+
+
+def splitIntoCharps(text):
+    charps = []
+    charpsz = []
+    charpsz = text.split("\n\n")
+    for ch in charpsz:
+        if ch == '':
+            pass
+        elif ch == '•':
+            pass
+        elif "page" in ch.lower():
+            pass
+        elif ch in charps:
+            pass
+        elif len(ch) < 4:
+            pass
+        else:
+            charps.append(ch)
+
+        
+    return charps
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: booktui file.pdf [OPTIONAL: page_number]\nKEYS: \n    -> = Next page\n    <- = Previous page\n    Q = exit")
+        sys.exit(1)
+    else:
+        file = sys.argv[1]
+        page = 0
+        if len(sys.argv) == 3:
+            page = sys.argv[2]
+        title = retrieveTitle(file)
+        content = retrieveContent(file)
+        book = splitIntoCharps(content)
+        ui.reader(book, title, page)
---
+++ ui.py
@@ -0,0 +1,55 @@
+import curses
+import textwrap
+
+def reader(chaptersx, title, page: int=0):
+    screen = curses.initscr()
+    screen.immedok(True)
+    screen.keypad(True)
+    curses.noecho()
+    chapters = chaptersx
+    pages = len(chapters)
+    while True:
+        screen.border(0)
+
+        box1 = curses.newwin(43, 193,4, 14)
+        box1.immedok(True)
+        box1.box()
+        box2 = curses.newwin(3, 13, 1, 2)
+        box2.immedok(True)
+        box2.box()
+        box3 = curses.newwin(3, 60, 1, 50)
+        box3.immedok(True)
+        box3.box()
+
+
+
+        
+        box2.addstr(1, 2, textwrap.fill(f"{str(page + 1)}/{str(pages)}", 10))
+        
+        box3.addstr(1, 2, textwrap.fill(f"{title}"))
+
+        box1.addstr(1, 3, textwrap.fill(chaptersx[page].replace(r"\n", "\n"), 180))
+
+
+        #box1.addstr("Hello World of Curses!")
+
+        char = screen.getch()
+        if char == 113: break
+        elif char == curses.KEY_LEFT:
+            page-=1
+            try:
+                x = chapters[page]
+            except:
+                page+=1
+        elif char == curses.KEY_RIGHT:
+            page+=1
+            try:
+                x = chapters[page]
+            except:
+                page-=1
+
+
+
+    
+    curses.endwin()
+
